caddd0371b81d5e6cdb7bea1f863edc6af42db6e
KoralopD4v <76450781+KoralopD4v@users.noreply.github.com>
2020-12-22 21:27:05 -0300

    Create CommandsMap.php

---
+++ src/LCore/Commands/CommandsMap.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LCore\Commands;
+
+use LCore\Main;
+use pocketmine\{ Player };
+
+class Commands{
+ /*
+  @return void
+ */
+ public static function init() : void{
+
+ }
+}
+
+?>
